66f90b3b46e10b585e1bd3e9c5c122093d3da5d9
Pranjal C <pranjalchakraborty@gmail.com>
2025-12-11 22:20:06 -0500

    Added tool callback and changed the database name

--- src/main/java/net/pranjal/testagent/config/ChatClientConfig.java
+++ src/main/java/net/pranjal/testagent/config/ChatClientConfig.java
@@ -9,9 +9,12 @@ import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.cassandra.CassandraChatMemoryRepository;
 import org.springframework.ai.google.genai.GoogleGenAiChatModel;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class ChatClientConfig {
@@ -39,16 +42,19 @@ public class ChatClientConfig {
     }
 
     @Bean
-    public ChatClient ollamaChatClient(OllamaChatModel chatModel, VectorStore vectorStore) {
+    public ChatClient ollamaChatClient(OllamaChatModel chatModel, VectorStore vectorStore, ToolCallbackProvider toolCallbackProvider) {
         ChatMemory chatMemory = MessageWindowChatMemory
                 .builder()
                 .chatMemoryRepository(chatMemoryRepository)
                 .maxMessages(100)
                 .build();
 
+        Arrays.stream(toolCallbackProvider.getToolCallbacks())
+                .forEach(c -> System.out.println(c.getToolDefinition()));
+
         return ChatClient
                 .builder(chatModel)
-                .defaultTools(new Tools())
+                .defaultToolCallbacks(toolCallbackProvider)
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(chatMemory).build(),
                         QuestionAnswerAdvisor.builder(vectorStore).build()
